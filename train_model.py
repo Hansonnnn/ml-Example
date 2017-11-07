1b32bc07e92d437d58f4f613f0056bc105c146a6
smithhan <1139562344@qq.com>
2017-11-07 10:57:11 +0800

    kmeans

--- train_model.py
+++ train_model.py
@@ -1,11 +1,12 @@
 """@description training with LM Classifier and cart classifier"""
 from keras.models import Sequential
-
 from keras.layers.core import Dense, Activation  # neural network ,activation function
 from common_util import get_path
 from handle_data import get_data
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
+from sklearn.cluster import KMeans
+import pandas as pd
 
 
 def train_lm_classification():
@@ -30,3 +31,20 @@ def train_cart_classification():
     train = get_data()[0]
     tree.fit(train[:, :3], train[:, 3])
     joblib.dump(tree, treefile)  # save training model by joblib
+
+
+def train_kmeans():
+    zscored_data_path = get_path('section1','zscored_data_path')
+    kmeans_file = get_path('section2', 'kmeans_file')
+    data = pd.read_excel(zscored_data_path)
+    kmodel = KMeans(n_clusters=5,n_jobs=4)
+    kmodel.fit(data)
+    # print(kmodel.cluster_centers_) ### five centres
+    # print(kmodel.inertia_)
+    kmodel.predict(data)
+
+    # print (kmodel.labels_)
+    joblib.dump(kmodel,kmeans_file)
+
+if __name__ == "__main__":
+    train_kmeans()
