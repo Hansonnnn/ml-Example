cbde530113d40555bb9a19849fd9e59f1a0df6d0
smithhan <1139562344@qq.com>
2017-12-26 22:08:12 +0800

    common utils

--- common_util.py
+++ common_util.py
@@ -2,16 +2,13 @@ import configparser
 import pandas as pd
 
 
-
-
 def get_path(section, path_name):
     conf = configparser.ConfigParser()
     conf.read('/Users/hanzhao/PycharmProjects/ml-example/file/config/ml.conf', encoding='utf-8')
     return conf.get(section, path_name)
 
 
-
-def get_data(self, file_name, sep=',', encoding='utf-8', **kwargs):
+def get_data(file_name, sep=',', encoding='utf-8', **kwargs):
     """@description withdraw data from csv
        @:param file_name  the file of csv formatter
        @:param sep  file's seperator  default is ',' 
@@ -25,8 +22,8 @@ def get_data(self, file_name, sep=',', encoding='utf-8', **kwargs):
     data = pd.read_csv(file_name, sep=sep, encoding=encoding)
     return data
 
-def write_file(self,file_path,text,encoding='utf-8'):
-    """@description  util with write file"""
-    with open(file_path,'w',encoding=encoding) as f:
-        f.write(text)
 
+def write_file(file_path, text, encoding='utf-8'):
+    """@description  util with write file"""
+    with open(file_path, 'w', encoding=encoding) as f:
+        f.write(text)
